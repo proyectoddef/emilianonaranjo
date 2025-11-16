07dd588346c2a800dfd02df006b0946c46b17240
proyectoddef <proyectoddef@gmail.com>
2025-11-16 19:57:32 -0300

    Delete en directory

--- en/js/version-loader.js
+++
@@ -1,9 +0,0 @@
-/* ===== CARGA AUTOMÁTICA DEL SISTEMA DE IDIOMAS ===== */
-
-(function () {
-  const version = "20251115"; // Cambiás este número solo cuando quieras forzar actualización
-
-  const script = document.createElement("script");
-  script.src = `js/lang-handler.js?v=${version}`;
-  document.head.appendChild(script);
-})();
